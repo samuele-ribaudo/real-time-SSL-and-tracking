5254e75b27a12f680974028ce0c57b34faf76fba
Sappu <samuele.ribaudo@tum.de>
2026-06-24 19:35:51 +0200

    Update DSP_pipeline function to include quiet room threshold
    Added a parameter to specify a quiet room threshold for audio processing.

--- code/Core/Inc/dsp_pipeline.h
+++ code/Core/Inc/dsp_pipeline.h
@@ -6,14 +6,15 @@
 
 /**
  * @brief Processes dual-channel audio buffers for TDOA.
- * @param[out] out_sample_offset  Pointer where the calculated signed lag will be stored.
- * @param[in]  left_mic           Pointer to Left channel ADC buffer.
- * @param[in]  right_mic          Pointer to Right channel ADC buffer.
- * @param[in]  size               Total number of samples in the buffers.
- * @param[in]  max_sample_lag     The maximum boundary for the cross-correlation search window.
+ * @param[out] out_sample_offset    Pointer where the calculated signed lag will be stored.
+ * @param[in]  quiet_room_treshold  Treshold value to check whether the room is quiet
+ * @param[in]  left_mic             Pointer to Left channel ADC buffer.
+ * @param[in]  right_mic            Pointer to Right channel ADC buffer.
+ * @param[in]  size                 Total number of samples in the buffers.
+ * @param[in]  max_sample_lag       The maximum boundary for the cross-correlation search window.
  * @return true if sound was detected and processed, false if no sound is detected.
  */
-bool DSP_pipeline(int16_t* out_sample_offset, 
+bool DSP_pipeline(int16_t* out_sample_offset, const uint16_t quiet_room_treshold,
                       const uint16_t* left_mic, const uint16_t* right_mic, 
                       const uint16_t size, const uint16_t max_sample_lag);
 
